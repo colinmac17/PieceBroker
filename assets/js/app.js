c73d79a35ac964548a9e7cecf066769980bae087
Colin McAtee <colinmac@umich.edu>
2017-06-24 12:26:27 -0500

    feature complet

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,29 @@ firebase.initializeApp(config);
 // reference database
 var database = firebase.database();
 
-// gather user input
+var apiKey = 'bd44f34a9f419b15bdda245f2c261942';
 
+//set global cityID
+var cityID;
+
+// gather user input
 $('#submitBtn').on('click', function(e) {
 	e.preventDefault();
+
+	var location = $('#locationInput').val();
+  	$('#locationInput').val('');
+	  var queryURL = `https://developers.zomato.com/api/v2.1/cities?q=${location}`;
+	  $.ajax({
+	    url: queryURL,
+	    method: 'GET',
+	    headers: {
+	      'user-key': apiKey
+	    }
+	  }).done(function(response) {
+	    console.log(response);
+	    cityID = response.location_suggestions[0].id;
+	    console.log(cityID);
+	    $('#welcomeMsg').html(`Welcome to ${location}!`);
+	    return cityID;
+	 });
 });
