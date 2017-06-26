a69f470c41bf2332298eeba0d0a37767b3a7bd65
Colin McAtee <colinmac@umich.edu>
2017-06-26 16:05:04 -0500

    fixed results table and added logic for budget click to check if an array is empty or not

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,23 +17,22 @@ firebase.initializeApp(config);
 // reference database
 var database = firebase.database();
 
-// restaurant name, restaurant address, restaurant cuisine type, restaurant budget, restaurant rating
-var recName, recAddress, recCuisine, recBudget, recRating, recDetails;
+// userResult, restaurant name, restaurant address, restaurant cuisine type, restaurant budget, restaurant rating
+var userResult, recName, recAddress, recCity, recCuisine, recBudget, recRating, recDetails;
 
 //set global vars for cityID, cuisineID 
 var cityID, cuisineID, userBudget, restaurantID;
 
+//set empty arrays for restaurant types
 var cheapRestaurants = [];
 var medRestaurants = [];
 var expensiveRestaurants = [];
 
 //get API Key from Firebase
 database.ref().once("value", function(snapshot) {
-  // do some stuff once
-  sv = snapshot.val();
-
-  apiKey = sv.apiKey;
-
+    var sv = snapshot.val();
+    //set value of apiKey
+    apiKey = sv.apiKey;
 });
 
 // gather user input
@@ -87,8 +86,8 @@ $(".gif").on("click", function() {
     }).done(function(response) {
         console.log(response);
         //For loop to push restaurants to budget arrays
-        for (var i=0; i < response.restaurants.length; i++) {
-            if(response.restaurants[i].restaurant.average_cost_for_two < 40) {
+        for (var i = 0; i < response.restaurants.length; i++) {
+            if (response.restaurants[i].restaurant.average_cost_for_two < 40) {
                 cheapRestaurants.push(response.restaurants[i]);
             } else if (response.restaurants[i].restaurant.average_cost_for_two >= 40 && response.restaurants[i].restaurant.average_cost_for_two < 80) {
                 medRestaurants.push(response.restaurants[i]);
@@ -103,18 +102,29 @@ $(".gif").on("click", function() {
 });
 
 $('.budget-gif').on('click', function() {
-    var userResult;
     userBudget = $(this).attr("data-id");
 
     if (userBudget === 'cheap') {
+        //check if there is a cheap restaurant
+        if (cheapRestaurants.length === 0 ){
+            alert('There are no Restaurants in this category');
+        }
         var cheapRandNum = Math.floor(Math.random() * cheapRestaurants.length);
         userResult = cheapRestaurants[cheapRandNum].restaurant;
         console.log(userResult);
     } else if (userBudget === 'medium') {
+        //check if there is a medium priced restaurant
+        if (medRestaurants.length === 0 ){
+            alert('There are no Restaurants in this category');
+        }
         var medRandNum = Math.floor(Math.random() * medRestaurants.length);
         userResult = medRestaurants[medRandNum].restaurant;
         console.log(userResult);
     } else {
+        //check if there is an expensive restaurant
+         if (expensiveRestaurants.length === 0 ){
+            alert('There are no Restaurants in this category');
+        }
         var expensiveRandNum = Math.floor(Math.random() * expensiveRestaurants.length);
         userResult = expensiveRestaurants[expensiveRandNum].restaurant;
         console.log(userResult);
@@ -123,6 +133,7 @@ $('.budget-gif').on('click', function() {
     //Set data for Database variables
     recName = userResult.name;
     recAddress = userResult.location.address;
+    recCity = userResult.location.city;
     recCuisine = userResult.cuisines;
     recBudget = userResult.average_cost_for_two;
     recRating = userResult.user_rating.aggregate_rating;
@@ -135,42 +146,11 @@ $('.budget-gif').on('click', function() {
     $('#recLink').attr('href', recDetails);
 
     //push data to firebase
-    addItemToFirebase(recName, recAddress, recCuisine, recBudget, recRating);
+    addItemToFirebase(recName, recAddress, recCity, recCuisine, recBudget, recRating);
 
     $('.price-container').fadeOut();
     $('.results-container').fadeIn();
 });
-
-// snapshot
-database.ref().on('child_added', function(childSnapshot){
-    var sv = childSnapshot.val();
-    //append elements to DOM
-    renderItem(sv.recName, sv.recAddress, sv.recCuisine, sv.recBudget, sv.recRating);
-});
-
-// add items to firebase
-function addItemToFirebase (name,address,cuisine,budget,rating) {
-    if (recName && recAddress && recCuisine && recBudget && recRating) {
-        database.ref().push({
-            recName: name,
-            recAddress: address,
-            recCuisine: cuisine,
-            recBudget: budget,
-            recRating: rating
-        });
-    console.log(`Name: ${recName} Address: ${recAddress} Cuisine: ${recCuisine} Budget: ${recBudget} Rating: ${recRating}`);
-    } else {
-        console.log('data missing in firebase');
-    };
-}
-
-// return items to DOM
-function renderItem (recName,recAddress,recCuisine,recBudget,recRating) {
-    var row = [recName,recAddress,recCuisine,recBudget,recRating].map(function (val) {
-        return "<td>" + val + "</td>"
-    }).join('');
-}
-
 
 //I'm Feeling Hungry click function
 $('#hungryBtn').on('click', function(e) {
@@ -192,7 +172,6 @@ $('#hungryBtn').on('click', function(e) {
 
     //Set Zomato Endpoint
     var locationURL = `https://developers.zomato.com/api/v2.1/cities?q=${location}`;
-    var searchURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}&count=10`;
     //Error message if user does not input data
     if (location.length < 1) {
         $('#failMsg').addClass('animated shake');
@@ -209,7 +188,7 @@ $('#hungryBtn').on('click', function(e) {
         }).done(function(response) {
             console.log(response);
             cityID = response.location_suggestions[0].id;
-
+            var searchURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}&count=20`;
             $.ajax({
                 url: searchURL,
                 method: 'GET',
@@ -218,12 +197,60 @@ $('#hungryBtn').on('click', function(e) {
                 }
             }).done(function(response) {
                 //get random restaurant name
-                console.log(response.restaurants[randNumRestaurant].restaurant.name);
+                userResult = response.restaurants[randNumRestaurant].restaurant;
+                console.log(userResult);
+                
+
+                //Set data for Database variables
+                recName = userResult.name;
+                recAddress = userResult.location.address;
+                recCity = userResult.location.city;
+                recCuisine = userResult.cuisines;
+                recBudget = userResult.average_cost_for_two;
+                recRating = userResult.user_rating.aggregate_rating;
+                recDetails = userResult.url;
+
+                //add data to results page
+                $('#recName').text(recName);
+                $('#recAddress').text(recAddress);
+                $('#recRating').text(recRating);
+                $('#recLink').attr('href', recDetails);
+
+                //push data to firebase
+                addItemToFirebase(recName, recAddress, recCity, recCuisine, recBudget, recRating);
+
+                $('.location-container').fadeOut();
+                $('.results-container').fadeIn();
             });
         });
     }
 });
-// grab title and fade
-// grab gifs and fade
+
+// show firebase data in user history table
+database.ref().on('child_added', function(childSnapshot) {
+    var sv = childSnapshot.val();
+    //append elements to DOM, except for APIKEY
+    if (sv.recName !== undefined) {
+    $('.active').removeClass('active');
+    $('.table-striped').prepend(`<tbody><tr><td class="td">${sv.recName}</td><td class="td">${sv.recAddress}</td><td class="td">${sv.recCity}</td><td class="td">${sv.recCuisine}</td><td class="td">$${sv.recBudget}</td><td class="td">${sv.recRating}</td></tr></tbody>`);
+    }
+});
 
 
+
+// function to add items to firebase
+function addItemToFirebase(name, address, city, cuisine, budget, rating) {
+    if (recName && recAddress && recCuisine && recBudget && recRating) {
+        database.ref().push({
+            recName: name,
+            recAddress: address,
+            recCity: city,
+            recCuisine: cuisine,
+            recBudget: budget,
+            recRating: rating
+        });
+        console.log(`Name: ${recName} Address: ${recAddress} Cuisine: ${recCuisine} Budget: ${recBudget} Rating: ${recRating}`);
+    } else {
+        console.log('data missing in firebase');
+    };
+}
