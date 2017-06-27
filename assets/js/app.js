b091377e3b631b0ae35b75888ed13a95ac7b9acc
Colin McAtee <colinmac@umich.edu>
2017-06-26 20:40:17 -0500

    added help modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -314,3 +314,21 @@ function medFail() {
 function expFail() {
     $('#expErrMsg').hide();
 }
+
+ //----- OPEN POP UP MODAL
+$('[data-popup-open]').on('click', function(e)  {
+    var targeted_popup_class = jQuery(this).attr('data-popup-open');
+    $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+    $('.footer').hide();
+ 
+    e.preventDefault();
+});
+ 
+    //----- CLOSE POP UP
+$('[data-popup-close]').on('click', function(e)  {
+    var targeted_popup_class = jQuery(this).attr('data-popup-close');
+    $('.footer').show();
+    $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+    e.preventDefault();
+});
