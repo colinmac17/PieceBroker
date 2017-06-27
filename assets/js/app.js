16b89391ba29d276e6d0ba23289da3f37d0d7407
Colin McAtee <colinmac@umich.edu>
2017-06-26 21:02:35 -0500

    added in popup modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -316,7 +316,7 @@ function expFail() {
 }
 
  //----- OPEN POP UP MODAL
-$('[data-popup-open]').on('click', function(e)  {
+$('.helpBtn').on('click', function(e)  {
     var targeted_popup_class = jQuery(this).attr('data-popup-open');
     $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
     $('.footer').hide();
