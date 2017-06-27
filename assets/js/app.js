0b040b78e796cdf3e997a0df0d8d60b21960600d
Colin McAtee <colinmac@umich.edu>
2017-06-26 21:12:42 -0500

    added modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -314,3 +314,22 @@ function medFail() {
 function expFail() {
     $('#expErrMsg').hide();
 }
+
+
+     //----- OPEN POP UP
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+        $('.footer').hide();
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE POP UP
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+        $('.footer').show();
+        e.preventDefault();
+     });
+
+});
