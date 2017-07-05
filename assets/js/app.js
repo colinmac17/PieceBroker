af55ffce7510b05fa1a605fdbdbab2c396b7e933
Colin McAtee <colinmac@umich.edu>
2017-07-05 11:05:14 -0500

    commenting out google maps api js until we add in the functionality tonight

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,26 @@
-// Grabs user location
-window.onload = function() {
-    var startPos;
-    var geoOptions = {
-        timeout: 10 * 1000
-    }
+// // Grabs user location
+// window.onload = function() {
+//     var startPos;
+//     var geoOptions = {
+//         timeout: 10 * 1000
+//     }
 
-    var geoSuccess = function(position) {
-        startPos = position;
-        document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-        document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-    };
-    var geoError = function(error) {
-        console.log('Error occurred. Error code: ' + error.code);
-        // error.code can be:
-        //   0: unknown error
-        //   1: permission denied
-        //   2: position unavailable (error response from location provider)
-        //   3: timed out
-    };
+//     var geoSuccess = function(position) {
+//         startPos = position;
+//         document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//         document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//     };
+//     var geoError = function(error) {
+//         console.log('Error occurred. Error code: ' + error.code);
+//         // error.code can be:
+//         //   0: unknown error
+//         //   1: permission denied
+//         //   2: position unavailable (error response from location provider)
+//         //   3: timed out
+//     };
 
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
+//     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+// };
 
 // // Google Maps Static API
 // https: //maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
