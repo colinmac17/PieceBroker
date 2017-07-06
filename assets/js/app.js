b128dca4a6f3e2ab090151e569d9861fac5c9425
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-07-05 19:06:55 -0500

    hi

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,6 @@
 //     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 // };
 
-// // Google Maps Static API
-// https: //maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-//     & markers = color: blue % 7 Clabel: S % 7 C40 .702147, -74.015794 & markers = color: green % 7 Clabel: G % 7 C40 .711614, -74.012318 & markers = color: red % 7 Clabel: C % 7 C40 .718217, -73.998284 & key = AIzaSyAFKkASmjO04PGg2KbBEOAlThg1rwd8Pkk
-
 // var DirectionsService = {
 //     DirectionsService.route() {
 //         origin: LatLng | String |,
