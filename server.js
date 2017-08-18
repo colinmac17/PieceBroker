d089dc86b019cf0e235edc3cf80d614fb66dba8c
Colin McAtee <colinmac@umich.edu>
2017-08-18 17:57:06 -0500

    updated structure

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, function(){
+    console.log(`App is up on PORT ${PORT}`);
+});
