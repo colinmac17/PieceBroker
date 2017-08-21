6ed6fbbac0cf655ac0f2ae2748c4bd1db59b8f8b
Colin McAtee <colinmac@umich.edu>
2017-08-21 18:34:29 -0500

    udpated server.js and imported routes

---
+++ controllers/usercontroller.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var router = express.Router();
+
+var user = require('../models/user');
+
+router.get('/test', function(req, res){
+    res.send('test');
+});
+
+//export router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
-
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
 //create express app
 var app = express();
 
-var PORT = process.env.PORT || 3000;
-
 app.use(express.static('./public'));
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/usercontroller');
+app.use('/', routes);
 
 app.listen(PORT, function(){
     console.log(`App is up on PORT ${PORT}`);
