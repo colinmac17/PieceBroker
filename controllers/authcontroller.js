dd4393394e2aa0a024c9a5e89ce1760e42ce8e25
Colin McAtee <colinmac@umich.edu>
2017-08-28 07:52:35 -0500

    made profile updates and post route updates on app.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,16 +1,11 @@
 var exports = module.exports = {}
 
-
 exports.signup = function(req,res){
 	res.render('signup'); 
 }
 
 exports.signin = function(req,res){
 	res.render('login'); 
-}
-
-exports.userpage = function(req,res){
-	res.render('profile', {users: req.users}); 
 }
 
 exports.logout = function(req,res){
