d967efe8e9c4cf2c35b222fd9a93c387e1c933a7
Colin McAtee <colinmac@umich.edu>
2017-08-29 20:04:53 -0500

    baaaap

--- public/js/app.js
+++ public/js/app.js
@@ -402,8 +402,7 @@ $('#hungryBtn').on('click', function(e) {
                 $('#results-container').show();
                 //set static map
                 var map = $('#themap');
-                var imgSrc = `https://maps.googleapis.com/maps/api/staticmap?size=400x400&maptype=roadmap
-                &format=jpg-baseline&markers=color:red%7Clabel:A%7C${userLatitude},${userLongitude}&markers=icon:https://goo.gl/eiJSZQ%7C${destLatitude},${destLongitude}&path=color:blue|weight:4|${userLatitude},${userLongitude}|${destLatitude},${destLongitude}&key=${googleApiKey}`;
+                var imgSrc = `https://www.google.com/maps/embed/v1/directions?key=${googleApiKey}&origin=${userLatitude},${userLongitude}&destination=${destLatitude},${destLongitude}&avoid=tolls|highways`;
                 $('.map-title').show();
                 $('#showMap').attr('src', imgSrc);
                 map.show();
